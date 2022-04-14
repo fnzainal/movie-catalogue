57a938896e8cda8c3c343b9cf176d1dae2da61f7
zainal <fnzainal@gmail.com>
2022-04-14 10:38:24 +0700

    fix bug fav icon

--- app/src/main/java/com/zainalfn/moviecatalogue/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/zainalfn/moviecatalogue/ui/detail/DetailActivity.kt
@@ -3,7 +3,6 @@ package com.zainalfn.moviecatalogue.ui.detail
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
