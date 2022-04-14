eed72242c4f713454acaa246978eb2a28f431956
zainal <fnzainal@gmail.com>
2022-04-14 13:28:17 +0700

    optimize import

--- app/src/main/java/com/zainalfn/moviecatalogue/ui/discover/tvshow/TvShowsFragment.kt
+++ app/src/main/java/com/zainalfn/moviecatalogue/ui/discover/tvshow/TvShowsFragment.kt
@@ -10,11 +10,11 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.zainalfn.core.data.source.local.entity.TYPE_TVSHOW
 import com.zainalfn.core.domain.model.Catalogue
+import com.zainalfn.core.ui.CatalogueAdapter
 import com.zainalfn.core.util.Status
 import com.zainalfn.core.util.gone
 import com.zainalfn.core.util.visible
 import com.zainalfn.moviecatalogue.databinding.FragmentListTvShowBinding
-import com.zainalfn.core.ui.CatalogueAdapter
 import com.zainalfn.moviecatalogue.ui.detail.DetailActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
